8cab73cd362ebf819e2ed2a4f19e7b05439df0fa
Pavan Yadav <45766022+pavank0723@users.noreply.github.com>
2024-10-13 19:16:04 +0530

    --UPDATE minor changes in main

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,6 @@ Future<void> main() async {
   // Ensure that the widget binding is initialized
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Check if the file exists
-  final envFile = File('.env');
-  if (await envFile.exists()) {
-    debugPrint("Found .env file!");
-  } else {
-    debugPrint("No .env file found!");
-  }
-
-  // Load the .env file before the app starts
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
